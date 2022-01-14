ca2579199e575ea355293abe005cf3827ee0d330
JackLais <jlais@bsd220.org>
2022-01-14 10:46:12 -0600

    Initial Commit

---
+++ Assignment NoteBook/Assignment_NoteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NoteBookApp.swift
+//  Assignment NoteBook
+//
+//  Created by Student on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NoteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
